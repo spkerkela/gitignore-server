d985e210b4620e34b5aa116888b96c7908825241
Simo-Pekka Kerkelä <simopekka1990@gmail.com>
2018-10-31 21:58:52 +0200

    initial commit

---
+++ index.js
@@ -0,0 +1,42 @@
+const express = require('express')
+const { exec, execSync } = require('child_process')
+
+const app = express()
+
+exec('joe ls', (exception, output, error) => {
+  if (exception) {
+    console.error(exception)
+    process.exit(1)
+  }
+  const supported = output
+    .split('\n')[1]
+    .split(',')
+    .map(s => s.trim())
+    .reduce((acc, curr) => {
+      const ignorefileContents = execSync('joe g ' + curr)
+      acc[curr] = ignorefileContents.toString('utf-8')
+      return acc
+    }, {})
+
+  app.listen(process.env.PORT, process.env.HOST, () => {
+    console.log(
+      'started server on ' + process.env.HOST + ':' + process.env.PORT,
+      supported
+    )
+  })
+
+  app.get('/', (req, res) => {
+    if (supported[req.query.lang]) {
+      res.send(supported[req.query.lang])
+    } else {
+      res.sendStatus(404)
+    }
+  })
+  app.get('/:lang', (req, res) => {
+    if (supported[req.params.lang]) {
+      res.send(supported[req.query.lang])
+    } else {
+      res.sendStatus(404)
+    }
+  })
+})
